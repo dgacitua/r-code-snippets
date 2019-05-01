800bcdb0aa85cdf6ad5000664d8fa1cca919586e
Daniel Gacitua <daniel.gacitua@usach.cl>
2019-05-01 15:44:40 -0400

    Fix parallel foreach

--- snippets/parallel-foreach.R
+++ snippets/parallel-foreach.R
@@ -1,13 +1,11 @@
 # Install libraries
 if(!require(here)) install.packages("here")
 if(!require(foreach)) install.packages("foreach")
-if(!require(parallel)) install.packages("parallel")
 if(!require(doSNOW)) install.packages("doSNOW")
 
 # Import libraries
 library(here)
 library(foreach)
-library(parallel)
 library(doSNOW)
 
 # Set working directory
@@ -21,24 +19,24 @@ registerDoSNOW(cl)
 cat("Running with", cores, "cores\n")
 
 # Example dataset
-df <- read.csv("datasets/iris.csv")
-head(df)
+ds <- read.csv("datasets/iris.csv")
+head(ds)
 
 # Progress bar
 par <- c()
-par$pb <- txtProgressBar(max = nrow(df), style = 3)
+par$pb <- txtProgressBar(max = nrow(ds), style = 3)
 par$progress <- function(n) setTxtProgressBar(par$pb, n)
 par$opts <- list(progress = par$progress)
 
 # Parallel foreach (results stored on "output" dataframe)
-output <- data.frame(foreach(i = 1:nrow(df), .combine = rbind, .inorder = FALSE, .packages = c("foreach"), .options.snow = par$opts) %dopar% {
+output <- data.frame(foreach(i = 1:nrow(ds), .combine = rbind, .inorder = FALSE, .packages = c("foreach"), .options.snow = par$opts) %dopar% {
   result <- c()
   
   result$id <-i
-  result$meanSL <- mean(df$SepalLengthCm)
-  result$meanSW <- mean(df$SepalWidthCm)
-  result$meanPL <- mean(df$PetalLengthCm)
-  result$meanPW <- mean(df$PetalWidthCm)
+  result$meanSL <- mean(ds$SepalLengthCm)
+  result$meanSW <- mean(ds$SepalWidthCm)
+  result$meanPL <- mean(ds$PetalLengthCm)
+  result$meanPW <- mean(ds$PetalWidthCm)
   
   return(result)
 })
