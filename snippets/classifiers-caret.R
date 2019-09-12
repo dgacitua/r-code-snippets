81d7c98a138ec941ae9725211a5c507fcf485f98
Daniel Gacitua <daniel.gacitua@usach.cl>
2019-09-12 19:30:24 -0300

    Add C5.0 classifier

--- snippets/classifiers-caret.R
+++ snippets/classifiers-caret.R
@@ -5,6 +5,7 @@ if(!require(dplyr)) install.packages("dplyr")
 if(!require(caret)) install.packages("caret")
 if(!require(parallel)) install.packages("parallel")
 if(!require(doSNOW)) install.packages("doSNOW")
+if (!require(C50)) install.packages('C50')
 if(!require(naivebayes)) install.packages("naivebayes")
 if(!require(randomForest)) install.packages("randomForest")
 if(!require(e1071)) install.packages("e1071")
@@ -17,6 +18,7 @@ library(dplyr)
 library(caret)
 library(parallel)
 library(doSNOW)
+library(C50)
 library(naivebayes)
 library(randomForest)
 library(e1071)
@@ -42,6 +44,17 @@ ds.cv <- trainControl(method = "cv", number = 10, savePredictions = "final", cla
 
 
 # Machine Learning Classifiers (Work In Progress)
+
+## Decision tree (C5.0)
+model.c50 <- c()
+run.c50 <- TRUE
+
+if (run.c50) {
+  model.c50$clsf <- train(class ~ ., data = ds, method = "C5.0", trControl = ds.cv)
+  model.c50$data <- model.c50$clsf$pred %>% arrange(rowIndex) %>% pull(pred)
+  model.c50$conf <- confusionMatrix(model.c50$data, ds$class, positive = "B")
+  print(model.c50$conf)
+}
 
 ## Naïve Bayes
 model.nb <- c()
